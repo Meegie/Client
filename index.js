44cd9ed4aba53ef05615fca1be8102b1012dc911
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-09-06 16:06:54 +0000

    Get job :D

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ const clientCPU = os.cpus().length;
 log(` | Client RAM: ${clientRAM} MB`);
 log(` | Client CPU: ${clientCPU} vCPU`);
 
+if (process.arch == 'x64') {
+    cpu = 'x86';
+} else if (process.arch == 'arm' || process.arch == 'arm64') {
+    cpu = 'arm';
+} else {
+    log(` | Unsupported cpu ${process.arch}`);
+    process.exit(1);
+}
+log(` | Client arch: ${cpu}`);
+
 // Get code
 log(`> Getting code...`);
 const code = process.env.CODE;
@@ -37,8 +47,43 @@ log(` | Created docker client!`);
 log(`> Connecting to API...`);
 main();
 
-async function main() {
-    const ok = await fetch(`${process.env.API}/connect?code=${code}`);
+let isWorking = false;
 
-    console.log(ok.ok, await ok.json());
+async function main() {
+    const ok = await fetch(`${process.env.API}/connect?code=${code}&cpu=${cpu}&cpucount=${clientCPU}&ram=${clientRAM}`);
+
+    if (ok.ok == false) {
+        log(` | Failed to connect: ${(await ok.json()).error}`);
+    }
+
+    log(` |  Connected!`);
+
+    setInterval(() => {
+        if (isWorking == false) getJob();
+    }, 1000*60*5);
+    getJob();
+}
+
+async function ping() {
+    console.log(`> Sending heartbeat...`);
+    await fetch(`${process.env.API}/ping?code=${code}`);
+}
+
+setInterval(() => {
+    if (isWorking == true) ping();
+}, 1000*60*1);
+setInterval(() => {
+    if (isWorking == false) ping();
+}, 1000*60*5);
+
+async function getJob() {
+    log(`> Getting job...`);
+
+    var job = await fetch(`${process.env.API}/jobs/get?code=${code}`).then(r => r.json());
+
+    if (job.found == false) {
+        return log(` | No job found :(`);
+    }
+
+    console.log(job);
 }
